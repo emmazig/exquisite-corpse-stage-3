ce80a2a19edcd2a1d522719b16786b7f70b310d2
Emma <162060188+emmazig@users.noreply.github.com>
2024-05-12 10:24:40 +1200

    Add files via upload

---
+++ README copy.md
@@ -0,0 +1,64 @@
+# Exquisite Corpse in Silico
+## 🔻 *time jockey* 🔻
+
+This README file contains:
+- Briefs for each stage of the assignment
+- Comments from each person who has developed this work
+
+**Please only edit the comments section for the stage of the assignmnent you are working on. Leave the rest of this file as it is.**
+
+*****
+## Briefs for each stage
+
+### 🔴 Stage 1
+Create a sketch that shows an unconventional representation of time. This 'clock' should be constantly changing, but its appearance can repeat in a subsequent cycle. It does not need to follow a 24-hour cycle (you can use any timescale). Give your sketch a unique title and edit the README file accordingly.
+
+### 🟡 Stage 2
+Respond to a Stage 1 sketch that you've chosen by using interactivity. You can either add to the chosen sketch or build a new one from scratch (this applies for all subsequent stages too).
+
+### 🟢 Stage 3
+Find or construct a dataset that relates to the sketch you've inherited. Incorporate visualisations of the dataset into your response to the Stage 2 sketch.
+
+### 🔵 Stage 4
+Adapt a Stage 3 sketch into a text-based game. There should be different pathways through the game. Images (still or moving) can accompany the words (but this is not required).
+
+### 🟣 Stage 5
+Reimagine something from a Stage 4 sketch as a living entity or an environment.
+
+*****
+## Comments for each stage
+
+### 🟥 Stage 1
+- Name/Alias:Kike
+- Github account:https://github.com/muscatscoconuts
+- Published webpage for stage 1:https://muscatscoconuts.github.io/exquisite-corpse-stage-one-time-jockey/
+
+*When i thought about time i thought of old film reels showing a galloping horse in motion, and as well as seeing the passage of time through destruction and degradation so i decided to combine the two. i kind of went for a 'crazy doomsday timer' aesthetic, and allowed the viewer to 'begin time' through their own interaction with the program. I used a combination of fund images from google for the horses, and photos of photos in a magazine i found on my computer of a collapsing building, and then set up a seconds counter for the doomsday vibe. i was aiming for the feeling of running through time lightheartedly despite the decay of the world as time passes on. *
+
+### 🟨 Stage 2
+- Name/Alias: Christina 
+- Github account:https://github.com/ChristinaLLLL
+- Published webpage for stage 2:https://christinallll.github.io/Stage-Two-Exquisite-Corpse-OverInteractivity/
+
+*Hearing Interactivity, I'm reminded of all the images and information shown to me throughout my day.  A lot of the information I don't want to see, but it is hard not to be forced to see images and ads everywhere.  I thought of the OVERACTIVITY of all the images on random.  We are overloaded with ideas, images, and information every few seconds in life.*  
+
+### 🟩 Stage 3
+- Name/Alias:
+- Github account:
+- Published webpage for stage 3:
+
+*... comments to be added here*
+
+### 🟦 Stage 4
+- Name/Alias:
+- Github account:
+- Published webpage for stage 4:
+
+*... comments to be added here*
+
+### 🟪 Stage 5
+- Name/Alias:
+- Github account:
+- Published webpage for stage 5:
+
+*... comments to be added here*
---
+++ sketch.js
@@ -0,0 +1,201 @@
+
+
+let imgs =[];// horse image array
+let horse1,horse2,horse3,horse4;
+let table,mix1,mix2,mix3;
+let checkbox;
+let joke,bored,quote,ip,temp,gentime;//API variables
+//API URLs
+let joke_api = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+let bored_api = 'https://www.boredapi.com/api/activity/';
+let quote_api = 'https://api.quotable.io/quotes/random?maxLength=50';
+let ip_api = 'https://api.ipify.org/?format=json';
+let weather_api = 'https://api.open-meteo.com/v1/forecast?latitude=-36.7611&longitude=175.4963&current=temperature_2m,rain,weather_code&hourly=temperature_2m,relative_humidity_2m,rain,showers,weather_code,cloud_cover,visibility&timezone=Pacific%2FAuckland';
+
+
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+}
+
+// API functions
+
+// random joke
+async function getJoke(){
+  let data = await fetch (joke_api);
+  let j_data = await data.json();
+  joke = j_data.joke;
+}
+//things to do when bored
+async function getBored(){
+  let data = await fetch (bored_api);
+  let j_data = await data.json();
+  bored = j_data.activity;
+}
+async function getQuote(){
+  let data = await fetch(quote_api);
+  let j_data = await data.json();
+  let result = j_data[0];
+  quote = result.content;
+}
+
+// IP address
+async function getIp(){
+  let data = await fetch (ip_api);
+  let j_data = await data.json();
+  ip = j_data.ip;
+}
+
+//Weather (temp and gereration time)
+async function getWeather(){
+  let data = await fetch (weather_api);
+  let j_data = await data.json();
+  temp = j_data.current.temperature_2m;
+  gentime =j_data.generationtime_ms;
+}
+
+
+
+
+
+function preload(){
+  //images and files
+  table = loadTable('Sheet1.csv','csv','header');
+  mix1 = loadImage ('images/mix1.png');
+  mix2 = loadImage ('images/mix2.png');
+  mix3 = loadImage ('images/mix3.png');
+  horse1 = loadImage('images/horse1.png');
+  horse2 = loadImage('images/horse2.png');
+  horse3 = loadImage('images/horse2.png');
+  horse4 = loadImage('images/horse4.png');
+
+
+}
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  textAlign(CENTER)
+  //starting checkbox
+  checkbox = createCheckbox();
+  checkbox.position(windowWidth/2, windowHeight/2);
+
+  // APIs
+  // getJoke();
+  // getBored();
+
+  getQuote();
+  // getIp();
+  // getWeather();
+
+  
+  pixelDensity(1);
+  frameRate(10);
+
+  // image opacity 
+  tint(255,129);
+  
+  imageMode(CENTER)
+  textAlign(CENTER)
+
+ //horse array
+  imgs.push(horse1);
+  imgs.push(horse2);
+  imgs.push(horse3);
+  imgs.push(horse4);
+//resize horse images
+  horse4.resize(300,0);
+  horse1.resize(300,0);
+  horse2.resize(300,0);
+  horse3.resize(300,0);
+
+  
+}
+
+//graphs from CSV
+function show(){
+
+//green graph
+  for( x = 0; x < table.getRowCount(); x++){
+    let row = table.getRow(x);
+    let counticons = row.get("green");
+    for (y = 0; y < counticons; y++){
+      noStroke();
+      fill (64, 179, 72);
+       rect( windowWidth/2 - 400 + x * 19, windowHeight/2 + 400 ,8, -counticons *random(0,40));
+    }
+    }
+//Blue graph
+    for( x = 0; x < table.getRowCount(); x++){
+      let row = table.getRow(x);
+      let counticons = row.get("blue");
+      for (y = 0; y < counticons; y++){
+        noStroke();
+        fill (41, 123, 230, 50);
+         rect( windowWidth/2 +150 + x * 30, windowHeight/4 + 120 ,20, -counticons * random(0,20));
+      }
+      }
+  
+  }
+
+   
+  
+
+
+function draw() {
+  background(100);
+
+// text opacity
+  fill(255,255,255,100)
+
+  // what happens if checkbox is checked
+if (checkbox.checked()){
+ 
+  // draw images 
+  image(mix3,windowWidth/2,windowHeight/2, 800,800);
+  image(mix1,windowWidth/2,windowHeight/2, 800,800);
+  image(mix2,windowWidth/2,windowHeight/2, 800,800);
+
+  show();
+
+  // data from APIs
+  fill(255,255,255,100)
+  textSize(50)
+  text(joke,windowWidth/2, windowHeight/2 + 200);
+  text(bored,windowWidth/2, windowHeight/2 - 100);
+  textSize(100)
+  
+  text(quote,windowWidth/2, windowHeight/2 - 100)
+  text(ip,windowWidth/2, windowHeight/2 -150)
+  text('current temperature:'+temp,windowWidth/2, windowHeight/2 +100)
+  text(gentime,windowWidth/2, windowHeight/2)
+
+  // extra text
+
+  // text('likes,comments,', windowWidth/2, windowHeight/2, - 200);
+  // text('4 unread messages',windowWidth/2 -200, windowHeight/2 - 300 );
+  // text('just posted', windowWidth/2 -80, windowHeight/2+ 290) ;
+  // text('likes, comments,',windowWidth/2, windowHeight/2 - 330 );
+  // text('likes, comments,', windowWidth/2, windowHeight/2+ 400);
+  // text('likes, comments,', windowWidth/2, windowHeight/2-200);
+  // text('2 unread messages',windowWidth/2, windowHeight/2- 400 );
+  
+  // minute timer
+
+  let s = second();
+  textSize(300);
+  fill(255);
+  text(s, windowWidth/2, windowHeight/2 - 50);
+
+  // horse images
+
+  let r = random(imgs);
+    image(r, windowWidth/2 + 200, windowHeight/2 + 300 );
+   
+}
+  
+
+  
+  }
+  
+
+  
+  
